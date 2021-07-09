972233f61508d4d8442b69d547fca308ca861123
Neha Aggarwal <neha68977@gmail.com>
2021-07-09 17:18:36 +0530

    First Commit

---
+++ js/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: false,
+    dots: true,
+    appendDots: ".slider-dots",
+    dotsClass: "dots",
+  });
+
+  let hamburger = document.querySelector(".hamburger");
+  let times = document.querySelector(".times");
+  let mobileNav = document.querySelector(".mobile-nav");
+  let home = document.querySelector(".home");
+  let about = document.querySelector(".about1");
+  let projects = document.querySelector(".projects1");
+  let work = document.querySelector(".work1");
+  let skills = document.querySelector(".skills1");
+  let contact = document.querySelector(".contact1");
+
+  hamburger.addEventListener("click", function () {
+    mobileNav.classList.add("open");
+  });
+
+  times.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+  });
+
+  home.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+  });
+
+  about.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+    mobileNav.classList.add(".about2");
+  });
+
+  projects.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+    mobileNav.classList.add(".projects2");
+  });
+
+  work.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+    mobileNav.classList.add(".work2");
+  });
+
+  skills.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+    mobileNav.classList.add(".skills2");
+  });
+
+  contact.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+    mobileNav.classList.add(".contact2");
+  });
+});
